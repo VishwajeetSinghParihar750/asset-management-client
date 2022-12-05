6764d6a27e3a1befa983b3fedfe8a7441fd936d6
VishwajeetSinghParihar750 <sVishwajeet750@gmail.com>
2022-12-06 02:40:11 +0530

    final commit

--- src/components/Dashboard/Assets.js
+++ src/components/Dashboard/Assets.js
@@ -84,6 +84,12 @@ function Assets(props) {
                   </th>
                   <th
                     scope="col"
+                    className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                  >
+                    Count
+                  </th>
+                  <th
+                    scope="col"
                     className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
                   >
                     Delete
